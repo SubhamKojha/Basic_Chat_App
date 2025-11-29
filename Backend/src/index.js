6e0389d1754bcb1df9fe66ce0f7e72a06aaaf7be
SubhamKojha <ojhas6667@gmail.com>
2025-11-29 20:42:31 +0530

    Project Done

---
+++ Backend/src/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "../src/lib/db.js";
+import authRoutes from "../src/routes/auth.route.js";
+import messageRoutes from "../src/routes/message.route.js";
+import { app, server } from "../src/lib/socket.js";
+
+import path from "path";
+
+dotenv.config();
+
+const __dirname = path.resolve();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+})
+);
+app.use(express.json());
+app.use(cookieParser());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+    console.log(`Server is listening at port ${PORT}`);
+    connectDB();
+});
